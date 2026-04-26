2c59f74581400c96b37379e59549648a91a9ffb6
RiklloD <lorikdalloshi@gmail.com>
2026-04-26 16:29:04 +0200

    fix gitignore et vercel output

--- apps/web/.vercel/output/functions/__server.func/_ssr/config-DL9xF4p6.mjs
+++
@@ -1,5 +0,0 @@
-import { e as env } from "./router-BK-LlCXj.mjs";
-const TENANT_SLUG = env.VITE_TENANT_SLUG;
-export {
-  TENANT_SLUG as T
-};
--- apps/web/.vercel/output/static/assets/config-Ds75Xnvb.js
+++
@@ -1,1 +0,0 @@
-import{g as T}from"./index-CC4rosrH.js";const N=T.VITE_TENANT_SLUG;export{N as T};
